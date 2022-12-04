c4f1783eacc39b7b006ce9945b4f61252da95a2f
Ramesh Banakar <ramesh.s.b@campusuvce.in>
2022-12-04 23:47:56 +0530

    adding engine version

--- server.js
+++ server.js
@@ -48,12 +48,12 @@ const server = new ApolloServer({
       : ApolloServerPluginLandingPageDisabled,
   ],
 });
-//if(process.env.NODE_ENV==="production"){
+if(process.env.NODE_ENV==="production"){
   app.use(express.static("client/build"))
   app.get("*",(req,res)=>{
     res.sendFile(path.resolve(__dirname,"client","build","index.html"))
   })
-//}
+}
 await server.start();
 server.applyMiddleware({ app, path: "/graphql" });
 httpServer.listen({ port: port }, ()=>{
