b65945743119b8db29c013f9594a997774e98eff
Ramesh Banakar <ramesh.s.b@campusuvce.in>
2022-12-04 23:21:44 +0530

    apollo express server adding

--- server.js
+++ server.js
@@ -1,44 +1,57 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 import { ApolloServer } from "apollo-server-express";
-import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+import {
+  ApolloServerPluginLandingPageGraphQLPlayground,
+  ApolloServerPluginDrainHttpServer,
+  ApolloServerPluginLandingPageDisabled,
+} from "apollo-server-core";
 import typeDefs from "./schemaGQL.js";
-import jwt from "jsonwebtoken"
-import dotenv from "dotenv"
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 import express from "express";
 import http from "http";
- const app = express();
- const httpServer = http.createServer(app);
+const app = express();
+const httpServer = http.createServer(app);
+const port =process.env.PORT||4000;
 //mongodb+srv://dbUser:dbUserPassword@cluster0.mhxw9ma.mongodb.net/?retryWrites=true&w=majority
-if(process.env.NODE_ENV!=='production'){
+if (process.env.NODE_ENV !== "production") {
   dotenv.config();
 }
 
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true })
-  .then(() => console.log("connection to db issuccess"))
+  .then(() => console.log("connection to db is success"))
   .catch((err) => console.log("error occured", err));
 
 //
 
-import "./models/quote.js"
-import "./models/user.js"
+import "./models/quote.js";
+import "./models/user.js";
 
 import resolver from "./resolvers.js";
 
 const server = new ApolloServer({
   typeDefs,
   resolvers: resolver,
-  context:({req})=>{
-    const {authorization}=req.headers;
-    if(authorization){
-      const {userId}=jwt.verify(authorization,process.env.JWT_SECRET)
-      return {userId}
+  context: ({ req }) => {
+    const { authorization } = req.headers;
+    if (authorization) {
+      const { userId } = jwt.verify(authorization, process.env.JWT_SECRET);
+      return { userId };
     }
-
   },
-  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+  plugins: [
+    ApolloServerPluginLandingPageGraphQLPlayground(),
+    process.env.NODE_ENV !== "production"
+      ? ApolloServerPluginDrainHttpServer({ httpServer })
+      : ApolloServerPluginLandingPageDisabled,
+  ],
 });
-
-server.listen(3000).then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
+await server.start();
+server.applyMiddleware({ app, path: "/graphql" });
+httpServer.listen({ port: port }, ()=>{
+    console.log(`🚀  Server ready at ${server.graphqlPath}`);
+})
+// server.listen().then(({ url }) => {
+  
+// });
